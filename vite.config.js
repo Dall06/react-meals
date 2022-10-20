d8a117e33b92e1e3e315cd8ccb3effb67248825f
Diego A. León <diegoa.leon@Diegos-MacBook-Pro.local>
2022-10-20 10:39:20 -0500

    fix: base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://dall06github.io/react-meals/',
+  base: 'https://dall06.github.io/react-meals/',
   plugins: [react()]
 });
